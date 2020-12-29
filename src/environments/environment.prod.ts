aef9922b2a4db63d3c7c2c13b8d062c217399718
Kapil Dev <talktokapildev@gmail.com>
2020-12-29 16:46:31 +0000

    netlify url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://clever-hugle-4ed991.netlify.app/.netlify/functions/s',
+  apiUrl: 'https://clever-hugle-4ed991.netlify.app/.netlify/functions/',
 };
